7cf3d1fcce9e6ccbd411a6dfb5ea9761d32cbc28
Derek Borges <dborges2143@gmail.com>
2022-03-27 19:05:19 -0400

    Week 6 and 7 Progress

--- README.md
+++ README.md
@@ -3,20 +3,26 @@
 
 Web app made with React to visualize cryptocurrency transactions across many exchanges.
 
-## Week 5 Progress
+## Week 6 Progress
 
-You can access the deployed app here: [Crypto Visualizer](https://react-crypto-visualizer.herokuapp.com/)
+### Generating a bubble for every transaction
 
-### Create a basic bubble web element
+For every transaction that is received over the WebSocket connection proved out in Week 4, the app now generates a bubble to display on the canvas.
 
-The [TransactionBubble](./src/components/TransactionBubble) component has been created based off the designs.
+### Refactor Canvas and Bubble Components
 
-### Add the functionality for generating bubbles of different sizes
+For performance reasons, the bubble from previous weeks did not perform well when the canvas was trying to generate many bubbles. I ended up going with a javascript canvas-renderer called [p5.js](https://p5js.org/). Consequently, this library also did not perform that well either. I think the reason is due to the fact that the socket is receiving a lot of transactions and it's difficult for the DOM to render that many animated components. For the purposes of the demo video, I limited the canvas to about 15-20 bubbles so that it could perform well enough to prototype the functionality. I hope to optimize the behavior in future weeks.
 
-There is a `Spawn Bubbles` button that displays on the bottom left of the page. When clicked, a bubble generates using a random number to show different sized bubbles being generated. There is a bubble counter on the bottom right of the screen.
+## Week 7 Progress
 
-- Note: the spawn button will be removed in the next iteration as the live cryptocurrency data will be generating the bubbles.
+### Complete the menu according to the design.
 
-### Add a basic animation for moving the bubbles down the screen
+The dropdown fields for selecting a cryptocurrency and the exchange have been added to the menu component. They are material dropdowns from [Material UI](https://v4.mui.com). Those were the dropdowns the most closely appeared like the fields in the designs.
 
-I originally wrote "down" but I realized that is not how real water bubbles work. When a bubble is generated, it is placed below the bottom of the screen and animates up using a CSS animation. After the bubble floats up and disappears above the screen, the bubble's web component is deleted from the DOM.
+### Make the menu able to be hidden or shown by the user.
+
+The menu can now be opened and closed. The menu animates in when opened and just disappears when the `X` is clicked at the top-left.
+
+### Add the ability to let the user change which cryptocurrency to watch.
+
+The app now responds to the newly added dropdown fields. When a user selects a different cryptocurrency or exchange, the socket updates the channels it's subscribed to in order to receive the desired trades that the user changed it to. Behind the scenes, the queue of transactions to create bubbles for is cleared out to make way for the new trades coming in.
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import './App.css';
 import Canvas from './components/Canvas/Canvas';
 import Menu from './components/Menu/Menu';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Transaction } from './models/transaction';
 
 function App() {
+  const [active, setActive] = useState(true)
+  const [exchange, setExchange] = useState('Coinbase')
+  const [currency, setCurrency] = useState('BTC')
+  const [subscription, setSubscription] = useState('')
+  const [initialCount, setInitialCount] = useState(0)
+  const [queue, setQueue] = useState([] as Transaction[])
+  const [newTransaction, setNewTransaction] = useState({} as Transaction)
+
   // Initiate WebSocket connection
   const wsBase = process.env.REACT_APP_WS_BASE_URL
   const apiKey = process.env.REACT_APP_API_KEY
@@ -14,40 +22,6 @@ function App() {
     sendMessage,
     readyState
   } = useWebSocket(`${wsBase}?api_key=${apiKey}`)
-  
-  // Respond to new messages coming in from the data provider
-  useEffect(() => {
-    if (lastMessage !== null) {
-      const data = JSON.parse(lastMessage.data)
-
-      // If the welcome message is received, the app is ready
-      // to request trade updates over the socket
-      // e.g. BTC-USD on Coinbase
-      if (data?.MESSAGE === 'STREAMERWELCOME') {
-        console.log('subscribing to channel...')
-        var subRequest = {
-          "action": "SubAdd",
-          "subs": ["0~Coinbase~BTC~USD"]
-        };
-        sendMessage(JSON.stringify(subRequest))
-
-      }
-      
-      // If the app receives a trade update,
-      // create a Transaction and log it to the console for now
-      else if (data?.TYPE === '0') {
-        const transaction: Transaction = {
-          exchange: data.M,
-          coin: data.FSYM,
-          price: data.P,
-          amount: data.Q
-        }
-        console.log(`${transaction.exchange}: ${transaction.amount} ${transaction.coin} for $${transaction.price}`);
-      }
-    }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lastMessage])
 
   // React to the state of the WebSocket connection
   // and just log it to the console for now
@@ -55,10 +29,103 @@ function App() {
     console.log('Connection is', readyState === ReadyState.OPEN ? 'ACTIVE' : 'INACTIVE')
   }, [readyState])
   
+  // Respond to new messages coming in from the data provider
+  useEffect(() => {
+    if (active && lastMessage) {
+      const data = JSON.parse(lastMessage.data)
+
+      // If the welcome message is received, the app is ready
+      // to request trade updates over the socket
+      if (data?.MESSAGE === 'STREAMERWELCOME') {
+        console.log('subscribing to trades...')
+        const newSub = `0~${exchange}~${currency}~USD`
+        var subRequest = {
+          "action": "SubAdd",
+          "subs": [newSub]
+        };
+        sendMessage(JSON.stringify(subRequest))
+        setSubscription(newSub)
+      }
+
+      // If the app receives a trade update,
+      // create a Transaction and log it to the console for now
+      else if (data?.TYPE === '0') {
+        if (initialCount <= 20) {
+          setInitialCount(initialCount+1)
+        } else {
+          const transaction: Transaction = {
+            exchange: data.M,
+            coin: data.FSYM,
+            price: data.P,
+            amount: data.Q
+          }
+
+          // Add it to the queue
+          setQueue([...queue, transaction])
+        }
+      }
+    }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastMessage])
+
+  useEffect(() => {
+    if (queue.length > 0) {
+      setNewTransaction(queue[0])
+
+      // Remove it from the queue
+      const queueCopy = queue
+      queue.splice(0, 1)
+      setQueue(queueCopy)
+    }
+  }, [queue])
+
+  useEffect(() => {
+    if (readyState === ReadyState.OPEN && subscription !== '') {
+      
+      // Check if it actually changed
+      const newSub = `0~${exchange}~${currency}~USD`
+      if (subscription !== newSub) {
+        // Remove current subscription
+        const removeSubRequest = {
+          "action": "SubRemove",
+          "subs": [subscription]
+        };
+        sendMessage(JSON.stringify(removeSubRequest))
+        
+        // Add new subscription
+        const addSubRequest = {
+          "action": "SubAdd",
+          "subs": [newSub]
+        };
+        sendMessage(JSON.stringify(addSubRequest))
+        setSubscription(newSub)
+        console.log('SUBSCRIPTION CHANGED FROM', subscription, 'TO', newSub)
+  
+        // Clear queue
+        setQueue([])
+      }
+
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency, exchange])
+
+  const clearNew = () => {
+    setNewTransaction({} as Transaction)
+  }
+
+  const onCurrencyChanged = (currency: string) => {
+    setCurrency(currency)
+  }
+
+  const onExchangeChanged = (exchange: string) => {
+    setExchange(exchange)
+  }
+
   return (
     <>
-      <Canvas />
-      <Menu />
+      <Canvas newTransaction={newTransaction} clearNew={clearNew} />
+      <Menu currencyChanged={onCurrencyChanged} exchangeChanged={onExchangeChanged} />
     </>
   );
 }
